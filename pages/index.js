cb7626208d736e207bca7668b251e3db15e93727
Danilo Elias <daniloselias@gmail.com>
2021-01-25 16:25:14 -0300

    Adicionar o link do repositório do projeto no github

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/danilose" />
+      <GitHubCorner projectUrl="https://github.com/danilose/aluraquiz" />
     </QuizBackground>
   );
 }
